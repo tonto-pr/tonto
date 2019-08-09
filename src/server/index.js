1d23cb66467190f1f19a27c33a482e2a33c277da
Anton Rautio <anton.rautio@smartly.io>
2019-08-09 16:51:02 +0300

    Initialize express app

---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var http = require('http');
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/read', function(req, res, next) {
+    res.json({'crud': 'READ'});
+})
+
+app.post('/create', function(req, res, next) {
+    res.json({'crud': 'CREATE'});
+})
+
+app.post('/update', function(req, res, next) {
+    res.json({'crud': 'UPDATE'});
+})
+
+app.delete('/delete', function(req, res, next) {
+    res.json({'crud': 'DELETE'});
+})
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log(`Listening on port ${port}`);
