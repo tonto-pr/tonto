85d4c89bedde9d72cc847bc27ec24c588eaf21bc
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-03-28 19:30:57 +0200

    Use different environment for local development

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config();
-
 let path;
 switch (process.env.NODE_ENV) {
   case "production":
     path = `${__dirname}/../.env`;
+    break;
+  case "development":
+    path = `${__dirname}/../.env.development`;
     break;
   default:
     path = `${__dirname}/../.env`;
