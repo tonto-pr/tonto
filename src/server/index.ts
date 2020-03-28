5e63ba3b150403a3c69d618c9417cc86e76f8a13
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-03-28 19:31:23 +0200

    Rename entity to fine

--- src/server/index.ts
+++ src/server/index.ts
@@ -16,71 +16,66 @@ mongoose.connect(DB_ADDRESS, {useNewUrlParser: true, useUnifiedTopology: true, d
 
 injectTypegooseDecorators();
 
-const EntityModel = getModelForClass(types.Entity);
-const EntityShellModel = getModelForClass(types.EntityShell);
+const FineModel = getModelForClass(types.Fine);
 const ErrorModel = getModelForClass(types.Error);
 
-const values: { [key: string]: types.Entity } = {};
+const values: { [key: string]: types.Fine } = {};
 
 const spec: api.Endpoints = {
-  '/entity': {
+  '/fine': {
     post: async ctx => {
-      values[ctx.body.value.id] = types.Entity.make({
+      values[ctx.body.value.id] = types.Fine.make({
         id: ctx.body.value.id,
         name: ctx.body.value.name
       }).success();
       return runtime.json(200, values[ctx.body.value.id]);
     }
   },
-  '/entity/{entityId}': {
+  '/fine/{fineId}': {
     delete: async ctx => {
-      const entity = values[ctx.params.entityId]
+      const fine = values[ctx.params.fineId]
 
-      if (entity) {
-        const temp_name = entity.name;
-        delete values[ctx.params.entityId];
-        return runtime.json(200, {id: ctx.params.entityId, name: temp_name});
+      if (fine) {
+        const temp_name = fine.name;
+        delete values[ctx.params.fineId];
+        return runtime.json(200, {id: ctx.params.fineId, name: temp_name});
       }
       return runtime.json(404, { message: 'not found', status: 404 });
     },
     get: async ctx => {
-      const entity = values[ctx.params.entityId];
-      if (entity) {
-        return runtime.json(200, entity);
+      const fine = values[ctx.params.fineId];
+      if (fine) {
+        return runtime.json(200, fine);
       }
       return runtime.json(404, { message: 'not found', status: 404 });
     },
     put: async ctx => {
-      const entity = values[ctx.params.entityId];
-      if (entity) {
-        return runtime.json(200, entity);
+      const fine = values[ctx.params.fineId];
+      if (fine) {
+        return runtime.json(200, fine);
       }
       return runtime.json(404, { message: 'not found', status: 404 });
     }
   },
-  '/entities': {
+  '/fines': {
     get: async ctx => {
-      const entities = await EntityModel.find({});
-      if (entities) {
-        return runtime.json(200, entities);
+      const fines = await FineModel.find({});
+      if (fines) {
+        return runtime.json(200, fines);
       }
       return runtime.json(404, { message: 'not found', status: 404 });
     }
   },
   '/test': {
     get: async ctx => {
-      const { _id: id } = await EntityModel.create({id: '12', name: 'hello'} as types.ShapeOfEntity); // an "as" assertion, to have types for all properties
-      const entity = await EntityModel.findById(id).exec();
+      const { _id: id } = await FineModel.create({id: '12', name: 'hello'} as types.ShapeOfFine); // an "as" assertion, to have types for all properties
+      const fine = await FineModel.findById(id).exec();
 
       const { _id: status } = await ErrorModel.create({status: 400, message: 'hello'} as types.ShapeOfError); // an "as" assertion, to have types for all properties
       const error = await ErrorModel.findById(status).exec();
       console.log(error)
-
-      const { _id: shellId } = await EntityShellModel.create({shellId: '3300', childEntity: {id: '11', name: 'child'}} as types.ShapeOfEntityShell); // an "as" assertion, to have types for all properties
-      const entityShell = await EntityShellModel.findById(shellId).exec();
-      console.log(entityShell)
       
-      return runtime.text(200, `${entity?.id} + ${entity?.name}`);
+      return runtime.text(200, `${fine?.id} + ${fine?.name}`);
     }
   }
 };
