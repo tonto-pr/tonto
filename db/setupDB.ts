eb3a1b109eef8b77ca5053e7dcd37d75e0c3932a
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-08-27 21:04:23 +0300

    Fix postgres initializatioN

--- db/setupDB.ts
+++ db/setupDB.ts
@@ -1,26 +1,28 @@
-import { POSTGRES_DB } from '../src/config';
+import { POSTGRES_DB } from "../src/config";
 
-const config = require('../knexfile')
+const config = require("../knexfile");
 
-const knexClient = require('knex')
-const knexInitializer = knexClient(config[process.env.NODE_ENV as string])
+const knexClient = require("knex");
+const knexInitializer = knexClient(config.fallback);
 
 async function initialiseDatabase() {
-  await knexInitializer.raw(`CREATE DATABASE ${POSTGRES_DB}`).catch((err: Error) => {
-    console.error(
-      { err },
-      'Something went wrong when initialising database. The database might exist already.'
-    )
-  })
+  await knexInitializer
+    .raw(`CREATE DATABASE ${POSTGRES_DB}`)
+    .catch((err: Error) => {
+      console.error(
+        { err },
+        "Something went wrong when initialising database. The database might exist already."
+      );
+    });
 
-  await knexInitializer.destroy()
+  await knexInitializer.destroy();
 }
 
 async function setupDatabase() {
-  await initialiseDatabase()
-  const knex = knexClient(config[process.env.NODE_ENV as string])
-  await knex.migrate.latest()
-  await knex.destroy()
+  await initialiseDatabase();
+  const knex = knexClient(config[process.env.NODE_ENV as string]);
+  await knex.migrate.latest();
+  await knex.destroy();
 }
 
-setupDatabase()
+setupDatabase();
