40c310c33120325d6a0a7ba40483aa0b3b0b58bf
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-09-06 14:36:26 +0300

    Return user group users with joined props

--- src/server/user_group.ts
+++ src/server/user_group.ts
@@ -32,16 +32,37 @@ export const userGroupEndpoints: api.Endpoints = {
   },
   "/user_group/users/add": {
     post: async (ctx) => {
-      const userGroupUsers = await Promise.all(
-        ctx.body.value.map((userGroupUser) => {
-          return knex("fact_user_group_users")
-            .returning("*")
-            .insert(userGroupUser as types.ShapeOfUserGroupUsers);
-        })
-      );
+      const userGroupUsers: types.ShapeOfUserGroupUsers[] = (
+        await Promise.all(
+          ctx.body.value.map((userGroupUser) => {
+            return knex("fact_user_group_users")
+              .returning("*")
+              .insert(userGroupUser) as Promise<types.ShapeOfUserGroupUsers>;
+          })
+        )
+      ).flat();
 
-      if (userGroupUsers.length > 0) {
-        return runtime.json(200, ctx.body.value);
+      const userGroupUsersWithProps: types.ShapeOfUserGroupUsersWithProps[] = (
+        await Promise.all(
+          userGroupUsers
+            .map(async (userGroupUser) => {
+              const { rows } = await knex.raw(`
+                SELECT row_to_json(dim_users) as user, row_to_json(dim_user_groups) as user_group, user_group_users_id
+                FROM fact_user_group_users
+                LEFT JOIN (SELECT user_id, username, email FROM dim_users) dim_users
+                ON dim_users.user_id = fact_user_group_users.user_id
+                LEFT JOIN dim_user_groups
+                ON dim_user_groups.user_group_id = fact_user_group_users.user_group_id
+                WHERE user_group_users_id = ${userGroupUser.user_group_users_id};
+              `);
+              return rows as types.ShapeOfUserGroupUsersWithProps;
+            })
+            .flat()
+        )
+      ).flat();
+
+      if (userGroupUsersWithProps.length > 0) {
+        return runtime.json(200, userGroupUsersWithProps);
       }
 
       return runtime.json(404, { message: "not found", status: 404 });
@@ -49,20 +70,44 @@ export const userGroupEndpoints: api.Endpoints = {
   },
   "/user_group/users/delete": {
     post: async (ctx) => {
-      const userGroupUsers = await Promise.all(
-        ctx.body.value.map((userGroupUser) => {
-          return knex("fact_user_group_users")
-            .returning("*")
-            .where({
-              user_group_id: userGroupUser.user_group_id,
-              user_id: userGroupUser.user_id,
+      const userGroupUsersWithProps: types.ShapeOfUserGroupUsersWithProps[] = (
+        await Promise.all(
+          ctx.body.value
+            .map(async (userGroupUser) => {
+              console.log(userGroupUser);
+              const { rows } = await knex.raw(`
+                SELECT row_to_json(dim_users) as user, row_to_json(dim_user_groups) as user_group, user_group_users_id
+                FROM fact_user_group_users
+                LEFT JOIN (SELECT user_id, username, email FROM dim_users) dim_users
+                ON dim_users.user_id = fact_user_group_users.user_id
+                LEFT JOIN dim_user_groups
+                ON dim_user_groups.user_group_id = fact_user_group_users.user_group_id
+                WHERE fact_user_group_users.user_group_id = ${userGroupUser.user_group_id}
+                AND fact_user_group_users.user_id = ${userGroupUser.user_id};
+              `);
+              console.log(rows);
+              return rows as types.ShapeOfUserGroupUsersWithProps;
             })
-            .del();
-        })
-      );
+            .flat()
+        )
+      ).flat();
+
+      const userGroupUsers: types.ShapeOfUserGroupUsers[] = (
+        await Promise.all(
+          ctx.body.value.map((userGroupUser) => {
+            return knex("fact_user_group_users")
+              .returning("*")
+              .where({
+                user_group_id: userGroupUser.user_group_id,
+                user_id: userGroupUser.user_id,
+              })
+              .del() as Promise<types.ShapeOfUserGroupUsers>;
+          })
+        )
+      ).flat();
 
       if (userGroupUsers.length > 0) {
-        return runtime.json(200, ctx.body.value);
+        return runtime.json(200, userGroupUsersWithProps);
       }
 
       return runtime.json(404, { message: "not found", status: 404 });
