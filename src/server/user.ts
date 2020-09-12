4bdca1ad73414d8f95a4bf5d4d900545fb56e69a
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-09-12 23:05:07 +0300

    Add created_at field for given fines

--- src/server/user.ts
+++ src/server/user.ts
@@ -122,7 +122,8 @@ export const userEndpoints: api.Endpoints = {
                 row_to_json(dim_users_giver) as giver_user,
                 row_to_json(dim_user_groups) as user_group,
                 row_to_json(dim_fines) as fine,
-                given_fine_id
+                given_fine_id,
+                created_at
               FROM fact_given_fines
               LEFT JOIN (SELECT user_id, username, email FROM dim_users) dim_users_receiver
               ON dim_users_receiver.user_id = fact_given_fines.receiver_user_id
